d5be78a43014a661487710aabc16022005369c40
rebelcl0ud <y0j0v33@gmail.com>
2024-05-14 23:16:25 -0500

    add: fallback ui/err handling

--- src/app/books/[id]/edit/page.tsx
+++ src/app/books/[id]/edit/page.tsx
@@ -1,9 +1,14 @@
 import EditBookForm from "@/components/EditBookForm";
 import { fetchBookByID } from "@/scripts/data";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const book = await fetchBookByID(id);
+
+  if (!book) {
+    notFound();
+  }
 
   return <EditBookForm book={book} />;
 }
